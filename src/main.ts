cfcf75e22f4a072473f72db432af8a46ad50ccc3
codewithalihamza <512syedalihamza@gmail.com>
2025-02-20 23:56:07 +0500

    Transforming Responses Interceptors can modify the response before sending it to the client.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { LoggingInterceptor } from './interceptor/logging-interceptor';
+import { TransformInterceptor } from './interceptor/transform-interceptor';
+// import { LoggingInterceptor } from './interceptor/logging-interceptor';
 
 async function bootstrap() {
   const port: number = 5000;
 
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new LoggingInterceptor());
+  // app.useGlobalInterceptors(new LoggingInterceptor());  // Global-Level Interceptor
+
+  //✅ This will wrap every response in { success: true, data: ... } format.
+  app.useGlobalInterceptors(new TransformInterceptor());  // Global-Level Interceptor 
+
   await app.listen(port, () => console.log(`Nest server ready at ${port}`));
 
 }
