3d060a0bb6577603e09027a6bd6e4f273069cb24
codewithalihamza <512syedalihamza@gmail.com>
2025-02-20 23:57:36 +0500

    Error Handling with Interceptors An interceptor can catch and handle exceptions globally.

---
+++ src/interceptor/error-handling-interceptor.ts
@@ -0,0 +1,21 @@
+import {
+    CallHandler,
+    ExecutionContext,
+    HttpException,
+    HttpStatus,
+    Injectable,
+    NestInterceptor,
+} from '@nestjs/common';
+import { Observable, catchError, throwError } from 'rxjs';
+
+@Injectable()
+export class ErrorHandlingInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        return next.handle().pipe(
+            catchError((err) => {
+                console.error('Error intercepted:', err);
+                return throwError(() => new HttpException('Something went wrong!', HttpStatus.INTERNAL_SERVER_ERROR));
+            }),
+        );
+    }
+}
